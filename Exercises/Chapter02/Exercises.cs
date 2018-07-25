53d92936607869e702137ec16dd1dbfdfecbf97e
timmyreilly <timreilly@live.com>
2018-07-24 17:20:01 -0700

    this stuff is tough

--- Exercises/Chapter02/Exercises.cs
+++ Exercises/Chapter02/Exercises.cs
@@ -1,14 +1,25 @@
-﻿namespace Exercises.Chapter2
-{
-   // 1. Write a console app that calculates a user's Body-Mass Index:
-   //   - prompt the user for her height in metres and weight in kg
-   //   - calculate the BMI as weight/height^2
-   //   - output a message: underweight(bmi<18.5), overweight(bmi>=25) or healthy weight
-   // 2. Structure your code so that structure it so that pure and impure parts are separate
-   // 3. Unit test the pure parts
-   // 4. Unit test the impure parts using the HOF-based approach
+﻿using System;
+using NUnit.Framework;
 
-   public static class Bmi
-   {
-   }
+namespace Exercises.Chapter2
+{
+    // 1. Write a console app that calculates a user's Body-Mass Index:
+    //   - prompt the user for her height in metres and weight in kg
+    //   - calculate the BMI as weight/height^2
+    //   - output a message: underweight(bmi<18.5), overweight(bmi>=25) or healthy weight
+    // 2. Structure your code so that structure it so that pure and impure parts are separate
+    // 3. Unit test the pure parts
+    // 4. Unit test the impure parts using the HOF-based approach
+    using static Math;
+    using static Console; 
+
+    public enum BmiRange { Underweight, Healthy, Overweight}
+
+    public static class Bmi
+    {
+        public static void Run()
+        {
+            Run(Read, Write); 
+        }
+    }
 }

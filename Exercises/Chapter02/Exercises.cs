0dbcccef84e863c6ec469c32c43b5f103bb59c7e
timmyreilly <timreilly@live.com>
2018-07-25 12:05:28 -0700

    Making moves

--- Exercises/Chapter02/Exercises.cs
+++ Exercises/Chapter02/Exercises.cs
@@ -11,15 +11,57 @@ namespace Exercises.Chapter2
     // 3. Unit test the pure parts
     // 4. Unit test the impure parts using the HOF-based approach
     using static Math;
-    using static Console; 
+    using static Console;
 
-    public enum BmiRange { Underweight, Healthy, Overweight}
+    public enum BmiRange { Underweight, Healthy, Overweight }
 
     public static class Bmi
     {
         public static void Run()
         {
-            Run(Read, Write); 
+            Run(Read, Write);
         }
+
+        internal static void Run(Func<string, double> read, Action<BmiRange> write)
+        {
+            // input
+            double weight = read("weight"), height = read("height");
+
+            var bmiRange = CalculateBmi(height, weight).ToBmiRange();
+
+            write(bmiRange);
+        }
+
+        internal static double CalculateBmi(double height, double weight)
+            => Round(weight / Pow(height, 2), 2);
+
+        internal static BmiRange ToBmiRange(this double bmi)
+            => bmi < 18.5 ? BmiRange.Underweight
+                : 25 <= bmi ? BmiRange.Overweight
+                : BmiRange.Healthy;
+
+        private static double Read(string field)
+        {
+            WriteLine($"Please enter your {field}");
+            return double.Parse(ReadLine());
+        }
+
+        private static void Write(BmiRange bmiRange)
+            => WriteLine($"Based on your BMI, you are {bmiRange}");
+
+    }
+
+    public class BmiTests
+    {
+        [TestCase(1.80, 77, ExpectedResult = 23.77)]
+        [TestCase(1.60, 77, ExpectedResult = 30.08)]
+        public double CalculateBmi(double height, double weight)
+            => Bmi.CalculateBmi(height, weight);
+
+        [TestCase(23.77, ExpectedResult = BmiRange.Healthy)]
+        [TestCase(30.08, ExpectedResult = BmiRange.Overweight)]
+        public BmiRange ToBmiRange(double bmi) => bmi.ToBmiRange(); 
+
+
     }
 }
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,11 +1,12 @@
 ﻿namespace Exercises
 {
-   public class Program
-   {
-      public static void Main(string[] args)
-      {
-         // run the program you've written, for example:
-         Chapter2.Solutions.Bmi.Run();
-      }
-   }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // run the program you've written, for example:
+            // Chapter2.Solutions.Bmi.Run();
+            Chapter2.Bmi.Run(); 
+        }
+    }
 }

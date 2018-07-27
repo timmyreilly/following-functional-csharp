e89d72897c5a6493206a850c2466fc9140cacc02
timmyreilly <timreilly@live.com>
2018-07-27 14:15:33 -0700

    like i said options are cool

--- Exercises/Chapter03/Exercises.cs
+++ Exercises/Chapter03/Exercises.cs
@@ -13,8 +13,9 @@ namespace Exercises.Chapter3
         // Enum.Parse<DayOfWeek>("Friday") // => Some(DayOfWeek.Friday)
         // Enum.Parse<DayOfWeek>("Freeday") // => None
         enum DayOfWeek { Monday, Tuesday, Wednesday }
-        Enum.Parse<DayOfWeek>("BBBBB");
 
+        string greet(Option<DayOfWeek> day) => 
+  
 
         // 2 Write a Lookup function that will take an IEnumerable and a predicate, and
         // return the first element in the IEnumerable that matches the predicate, or None
@@ -23,6 +24,10 @@ namespace Exercises.Chapter3
         // bool isOdd(int i) => i % 2 == 1;
         // new List<int>().Lookup(isOdd) // => None
         // new List<int> { 1 }.Lookup(isOdd) // => Some(1)
+
+        static bool isOdd(int i) => i % 2 == 1; 
+
+
 
         // 3 Write a type Email that wraps an underlying string, enforcing that it’s in a valid
         // format. Ensure that you include the following:

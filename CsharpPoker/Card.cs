94c72907050280fd463e8661ca70efb01a948c38
timmyreilly <timreilly@live.com>
2018-08-20 18:44:47 -0700

    Control Alt Space to turn of suggestion mode... how the f was I supposed to know that.

--- CsharpPoker/Card.cs
+++ CsharpPoker/Card.cs
@@ -6,36 +6,40 @@ namespace CsharpPoker
 {
     public enum CardValue
     {
-        Two = 2, 
-        Three, 
-        Four, 
-        Five, 
-        Six, 
-        Seven, 
-        Eight, 
-        Nine, 
-        Ten, 
-        Jack, 
-        Queen, 
-        King, 
+        Two = 2,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
         Ace
     }
 
     public enum CardSuit
     {
-        Spades, 
-        Diamonds, 
-        Clubs, 
-        Hearts 
+        Spades,
+        Diamonds,
+        Clubs,
+        Hearts
     }
     public class Card
     {
         public Card(CardValue value, CardSuit suit)
         {
             Value = value;
-            Suit = suit; 
+            Suit = suit;
         }
-        public CardValue Value { get; set; }
-        public CardSuit Suit { get; set; } 
+        public CardValue Value { get; }
+        public CardSuit Suit { get; }
+
+        public override string ToString()
+            => $"{Value} of {Suit}";
+
     }
 }
--- CsharpPoker/Tests/CardTests.cs
+++ CsharpPoker/Tests/CardTests.cs
@@ -13,14 +13,31 @@ namespace CsharpPoker.Tests
             var card = new Card(CardValue.Ace, CardSuit.Clubs);
             Assert.Equal(CardSuit.Clubs, card.Suit);
             Assert.Equal(CardValue.Ace, card.Value);
-            // Assert.Equal("0", "1"); 
-
         }
 
         [Fact]
-        public void CanFail()
+        public void CanDescribeCard()
         {
-            Assert.Equal("1", "1");
+            var card = new Card(CardValue.Ace, CardSuit.Spades);
+
+            Assert.Equal("Ace of Spades", card.ToString()); 
+        }
+
+        [Fact]
+        public void CanCreateHand()
+        {
+            var hand = new Hand();
+            Assert.False(hand.Cards.Any());
+        }
+
+        [Fact]
+        public void CanHandDrawCard()
+        {
+            var card = new Card(CardValue.Ace, CardSuit.Spades);
+            // var hand 
+            var hand = new Hand();
+            
+
         }
     }
 }

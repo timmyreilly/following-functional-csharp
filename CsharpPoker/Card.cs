3854fc1856dbfc517913fd6df7b8e0758f1db183
timmyreilly <timreilly@live.com>
2018-08-21 11:21:47 -0700

    Sort of works... did you know you can press shift enter to jump to the next line without messing up your brackets.

--- CsharpPoker/Card.cs
+++ CsharpPoker/Card.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CsharpPoker
@@ -28,6 +29,20 @@ namespace CsharpPoker
         Clubs,
         Hearts
     }
+
+    public enum HandRank
+    {
+        HighCard,
+        Pair, 
+        TwoPair,
+        ThreeOfAKind,
+        Straight,
+        Flush,
+        FullHouse,
+        FourOfAKind,
+        StraightFlush,
+        RoyalFlush
+    }
     public class Card
     {
         public Card(CardValue value, CardSuit suit)
@@ -41,5 +56,55 @@ namespace CsharpPoker
         public override string ToString()
             => $"{Value} of {Suit}";
 
+    }
+
+    public class Hand
+    {
+        private readonly List<Card> cards = new List<Card>(); 
+
+        public IEnumerable<Card> Cards { get { return cards; } }
+        public Hand()
+        {
+            cards = new List<Card>();
+        }
+
+        public void Draw(Card card)
+        {
+            cards.Add(card);
+
+        }
+
+        public Card HighCard()
+        {
+            Card highCard = cards.First();
+            foreach (var nextCard in Cards)
+            {
+                if (nextCard.Value > highCard.Value)
+                {
+                    highCard = nextCard;
+                }
+
+            }
+            return highCard;
+
+        }
+
+        public HandRank GetHandRank()
+        {
+            if (HasRoyalFlush()) return HandRank.RoyalFlush;
+            if (HasFlush()) return HandRank.Flush;
+            return HandRank.HighCard; 
+
+        }
+
+        private bool HasFlush()
+        {
+            return cards.All(c => cards.First().Suit == c.Suit); 
+        }
+
+        public bool HasRoyalFlush()
+        {
+            return HasFlush() && cards.All(c => c.Value > CardValue.Nine); 
+        }
     }
 }

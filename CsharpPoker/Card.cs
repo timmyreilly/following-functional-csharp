3b4d83ab3c03fba4a35c9ceae08d9b4d1bf01cdd
timmyreilly <timreilly@live.com>
2018-08-21 11:36:29 -0700

    Yay Nicely Refactored

--- CsharpPoker/Card.cs
+++ CsharpPoker/Card.cs
@@ -62,19 +62,11 @@ namespace CsharpPoker
     {
         private readonly List<Card> cards = new List<Card>(); 
 
-        public IEnumerable<Card> Cards { get { return cards; } }
-        public Hand()
-        {
-            cards = new List<Card>();
-        }
+        public IEnumerable<Card> Cards => cards;
 
-        public void Draw(Card card)
-        {
-            cards.Add(card);
+        public void Draw(Card card) => cards.Add(card); 
 
-        }
-
-        public Card HighCard()
+        public Card HighCardOld()
         {
             Card highCard = cards.First();
             foreach (var nextCard in Cards)
@@ -89,7 +81,20 @@ namespace CsharpPoker
 
         }
 
-        public HandRank GetHandRank()
+        public Card HighCardOldy()
+        {
+            return cards.Aggregate((highCard, nextCard) => nextCard.Value > highCard.Value ? nextCard : highCard); 
+        }
+
+        public Card HighCard() => cards.Aggregate((highCard, nextCard) => nextCard.Value > highCard.Value ? nextCard : highCard);
+
+        // Ternary operators instead of return early. 
+        public HandRank GetHandRank() =>
+            HasRoyalFlush() ? HandRank.RoyalFlush :
+            HasFlush() ? HandRank.Flush :
+            HandRank.HighCard;
+
+        public HandRank GetHandRankOldest()
         {
             if (HasRoyalFlush()) return HandRank.RoyalFlush;
             if (HasFlush()) return HandRank.Flush;
@@ -97,14 +102,22 @@ namespace CsharpPoker
 
         }
 
-        private bool HasFlush()
-        {
-            return cards.All(c => cards.First().Suit == c.Suit); 
-        }
 
-        public bool HasRoyalFlush()
+        private bool HasFlush() => cards.All(c => cards.First().Suit == c.Suit); 
+
+
+        public bool HasRoyalFlush() => HasFlush() && cards.All(c => c.Value > CardValue.Nine);
+
+
+
+
+        public bool HasRoyalFlushOldest()
         {
             return HasFlush() && cards.All(c => c.Value > CardValue.Nine); 
+        }
+        private bool HasFlushOldest()
+        {
+            return cards.All(c => cards.First().Suit == c.Suit); 
         }
     }
 }

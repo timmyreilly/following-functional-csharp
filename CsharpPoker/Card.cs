d984901d17993fbed9ee04f214bbaaded95bb0be
timmyreilly <timreilly@live.com>
2018-08-20 17:28:29 -0700

    Yay Tests ctrl+r, l to re-run last test.

---
+++ CsharpPoker/Card.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpPoker
+{
+    public enum CardValue
+    {
+        Two = 2, 
+        Three, 
+        Four, 
+        Five, 
+        Six, 
+        Seven, 
+        Eight, 
+        Nine, 
+        Ten, 
+        Jack, 
+        Queen, 
+        King, 
+        Ace
+    }
+
+    public enum CardSuit
+    {
+        Spades, 
+        Diamonds, 
+        Clubs, 
+        Hearts 
+    }
+    public class Card
+    {
+        public Card(CardValue value, CardSuit suit)
+        {
+            Value = value;
+            Suit = suit; 
+        }
+        public CardValue Value { get; set; }
+        public CardSuit Suit { get; set; } 
+    }
+}
--- CsharpPoker/Tests/CardTests.cs
+++ CsharpPoker/Tests/CardTests.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace CsharpPoker.Tests
 {
     public class CardTests
     {
+        [Fact]
+        public void CanCreateCardWithValue()
+        {
+            var card = new Card(CardValue.Ace, CardSuit.Clubs);
+            Assert.Equal(CardSuit.Clubs, card.Suit);
+            Assert.Equal(CardValue.Ace, card.Value);
+            // Assert.Equal("0", "1"); 
+
+        }
+
+        [Fact]
+        public void CanFail()
+        {
+            Assert.Equal("1", "1");
+        }
     }
 }

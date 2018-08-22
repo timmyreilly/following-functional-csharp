3b12654dec4a7f7f784bf4155507e7b579ff8b2c
timmyreilly <timreilly@live.com>
2018-08-22 11:01:39 -0700

    What is wrong...

---
+++ Examples/Chapter05/Class.cs
@@ -0,0 +1,25 @@
+﻿using Examples.Chapter3;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LaYumba;
+using LaYumba.Functional; 
+
+namespace Examples.Chapter05
+{
+    public class Class
+    {
+    }
+
+    [TestCase(ExpectedResult = 75000)]
+    public decimal AverageEarningsOfRichestQuartile()
+    {
+        var population = Range(1, 8)
+            .Select(i => new Person { Earnings = 1 * 1000 })
+            .ToList();
+
+        return PopulationStatistics.AverageEarningsOfRichestQuartile(population);
+    }
+}
